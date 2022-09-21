279664d8a00f26bc3a8d5cd5848623a601258614
Dmitrii Konevnin <dmitrii.konevnin@gmail.com>
2022-09-21 13:17:29 +0300

    Start project initial commit

---
+++ src/component/header/header.tsx
@@ -0,0 +1,13 @@
+function Header() {
+    return (
+        <ul className="nav-link">
+            <li className="nav-link-item link-header">1</li>
+            <li className="nav-link-item link-header">2</li>
+            <li className="nav-link-item link-header">3</li>
+            <li className="nav-link-item link-header">4</li>
+            <li className="nav-link-item link-header">5</li>
+        </ul>
+    );
+}
+
+export default Header;
---
+++ src/pages/main/main.tsx
@@ -0,0 +1,14 @@
+function Main() {
+  return (
+    <>
+      <div className="layer-back-page"></div>
+      <h2 className="sub-main-title">Sub Main Title</h2>
+      <h1 className="main-title-page">Title Pages</h1>
+      <div className="layer-center-page"></div>
+      <div className="layer-page-front"></div>
+    </>
+
+  );
+}
+
+export default Main;
